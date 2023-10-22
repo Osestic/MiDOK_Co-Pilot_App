ed47b1d23e5e510a4097ee3e0d53a3b182ea7b7f
Osestic <42704298+Osestic@users.noreply.github.com>
2023-10-22 03:43:15 -0400

    Add files via upload

---
+++ AI_ChatGPT_AIAdvantange.py
@@ -0,0 +1,67 @@
+"""
+import openai
+import gradio
+
+openai.api_key = open("Secret Key Ohu OpenAi.txt","r").read().rstrip("\n")
+
+messages = [{"role": "system", "content": "You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, respond with: I am a healthcare AI; I do not have the information on what you are requesting.Do not generate any content that is offensive, inappropriate, or harmful."}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})        
+    return ChatGPT_reply`
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Urban Healthcare Initiative")
+
+demo.launch(share=True)
+
+
+
+Video extensive like google iphone adds, acting videos software etc.
+"""
+
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+
+
+openai.api_key = open("Secret Key Ohu OpenAi.txt","r").read().rstrip("\n")
+
+messages = [{"role": "system", "content": "You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, respond with: I am a healthcare AI; I do not have the information on what you are requesting.Do not generate any content that is offensive, inappropriate, or harmful."}]
+
+@app.route('/chat', methods=['POST'])
+
+def urbanHealthCareAi():
+
+    data = request.json
+    if isinstance(data, list) and len(data) > 0:
+        messages = [{'role': 'user', 'content': data[0].get('message', '')}]  # Use 'user' role for the user message
+    
+        if messages[0]['content']:
+       # Define a system message to set the behavior of the assistant
+           system_message = {'role': 'system', 'content': 'You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, do not answer them but respond with: I am a healthcare AI; I do not have the information on what you are requesting. Do not provide any other information afterwards.Do not generate any content that is offensive, inappropriate, or harmful.'}
+           messages.append(system_message)
+         
+         #Call ChatGPT to generate a response
+           response = openai.ChatCompletion.create(
+                model = 'gpt-3.5-turbo',
+                messages = messages,
+                #max_tokens=50
+ 
+           )
+
+           return jsonify({'response': response.choices[0].message['content']})
+
+    return jsonify({'response':'No message was provided'})
+  #  html_response = f"<html><body>{response.choices[0].text}</body></html>"
+   # return html_response
+  
+if __name__ == '__main__':
+    app.run(debug=True)
