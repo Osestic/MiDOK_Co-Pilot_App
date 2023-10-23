b4a573edc0aed391a6d9e9b209c0c7969f1c3123
Osestic <42704298+Osestic@users.noreply.github.com>
2023-10-23 01:03:09 -0400

    Update start_api.py

--- start_api.py
+++ start_api.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the command to start your ChatGPT API
-command = ["/opt/render/project/src/.venv/bin/python", "start_api.py"]
+command = ["/opt/render/project/src/.venv/bin/python", "AI_ChatGPT_AIAdvantange.py"]
 
 # Run the command
 subprocess.run(command, check=True)
