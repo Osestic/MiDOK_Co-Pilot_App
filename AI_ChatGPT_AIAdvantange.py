9621a3df8037d29cf2a1fd247f217684f2e7e8e3
Osestic <42704298+Osestic@users.noreply.github.com>
2023-10-23 16:22:48 -0400

    Update AI_ChatGPT_AIAdvantange.py
    Had to change how json data is received by the API

--- AI_ChatGPT_AIAdvantange.py
+++ AI_ChatGPT_AIAdvantange.py
@@ -1,4 +1,4 @@
-
+"""
 from flask import Flask, request, jsonify
 import openai
 
@@ -36,6 +36,47 @@ def urbanHealthCareAi():
     return jsonify({'response':'No message was provided'})
     #html_response = f"<html><body>{response.choices[0].text}</body></html>"
    # return html_response
-  
+  """
+from flask import Flask, request, jsonify
+import openai
+
+
+app = Flask(__name__)
+
+
+
+openai.api_key = open("Secret Key Ohu OpenAi.txt","r").read().rstrip("\n")
+
+messages = [{"role": "system", "content": "You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, respond with: I am a healthcare AI; I do not have the information on what you are requesting.Do not generate any content that is offensive, inappropriate, or harmful."}]
+
+@app.route('/chat', methods=['POST']) 
+
+def urbanHealthCareAi():
+
+    data = request.json
+    messages = []  
+    
+    if 'message' in data:
+           user_message = data['message']
+           messages.append({'role': 'user', 'content': user_message})
+
+       # Define a system message to set the behaviour of the assistant
+           system_message = {
+               'role': 'system',
+              'content': 'You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, do not answer them but respond with: I am a healthcare AI; I do not have the information on what you are requesting. Do not provide any other information afterwards.Do not generate any content that is offensive, inappropriate, or harmful.'
+              }
+
+           messages.append(system_message)
+         
+         #Call ChatGPT to generate a response
+           response = openai.ChatCompletion.create(
+                model = 'gpt-3.5-turbo',
+                messages = messages,
+                #max_tokens=50
+ 
+           )
+
+           return jsonify({'response': response.choices[0].message['content']})
+        
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
