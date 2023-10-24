5c1f0dbfbdde4dd53c18c3e5f7d25f1776eeeb73
Osestic <42704298+Osestic@users.noreply.github.com>
2023-10-24 01:09:53 -0400

    Update AI_ChatGPT_AIAdvantange.py

--- AI_ChatGPT_AIAdvantange.py
+++ AI_ChatGPT_AIAdvantange.py
@@ -39,30 +39,38 @@ def urbanHealthCareAi():
   """
 from flask import Flask, request, jsonify
 import openai
-
+import tiktoken
 
 app = Flask(__name__)
-
 
 
 openai.api_key = open("Secret Key Ohu OpenAi.txt","r").read().rstrip("\n")
 
 messages = [{"role": "system", "content": "You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, respond with: I am a healthcare AI; I do not have the information on what you are requesting.Do not generate any content that is offensive, inappropriate, or harmful."}]
 
+
+ 
+
+#POST request route
 @app.route('/chat', methods=['POST']) 
 
 def urbanHealthCareAi():
-
+    num_tokens = 0 
     data = request.json
     messages = []  
     
     if 'message' in data:
-           user_message = data['message']
-           messages.append({'role': 'user', 'content': user_message})
 
+           user_message = data['message']
+           
+           num_tokens = num_tokens_from_string(user_message, 'gpt-3.5-turbo')
+          
+           messages.append({'role': 'user', 'content': user_message})
+          
+           
        # Define a system message to set the behaviour of the assistant
            system_message = {
-               'role': 'system',
+              'role': 'system',
               'content': 'You are the best and most helpful healthcare AI with decades of years of information and data. I am only interested in information related to healthcare. Do not provide any information that is unrelated to healthcare. If a user asks a question unrelated to healthcare, do not answer them but respond with: I am a healthcare AI; I do not have the information on what you are requesting. Do not provide any other information afterwards.Do not generate any content that is offensive, inappropriate, or harmful.'
               }
 
@@ -72,11 +80,21 @@ def urbanHealthCareAi():
            response = openai.ChatCompletion.create(
                 model = 'gpt-3.5-turbo',
                 messages = messages,
+                temperature = 0,
                 #max_tokens=50
  
            )
 
-           return jsonify({'response': response.choices[0].message['content']})
-        
+           print("\nThe number of tokens in your message is", num_tokens)
+           
+           return jsonify({'response': response.choices[0].message['content']}),200
+   
+def num_tokens_from_string(string: str, encoding_for_model: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.encoding_for_model(encoding_for_model)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
