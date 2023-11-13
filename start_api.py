d4c0254fcb868f32511316d6a885e76f9b3f465a
Orieka.O.S.E <agholor2004@gmail.com>
2023-11-12 21:31:45 -0500

    Delete start_api.py

--- start_api.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Define the command to start your ChatGPT API
-command = ["/opt/render/project/src/.venv/bin/python", "start_api.py"]
-
-# Run the command
-subprocess.run(command, check=True)
